3ec3c8e1183667d7221d35cd650a59e9c1db102f
Abhishek Kumar <ak23@iitbbs.ac.in>
2018-05-27 16:33:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,96 +1,29 @@
-(function (){
-'use strict';
-
-angular.module('MyPage',['ngAnimate'])
-.controller('FrameController',FrameController)
+$(document).ready(function(){
 
 
-
-function FrameController($scope,$window){
-
-  $scope.name = 'home';
-  $scope.color = {
-    'home':'#2F4F4F',
-    'projects':'#2D3A4D',
-    'skills':'#00FA9A',
-    'about':'#48D1CC'
-  };
-  $scope.wbl = 20;
-
-  $scope.completetion = {
-    html:0.9,
-    css:0.9,
-    js:0.8,
-    jquery:0.8,
-    bootstrap:0.67,
-    android:0.7,
-    java:0.7,
-    c:0.85,
-    python:0.8,
-    git:0.68,
-    ng:0.8,
-    sql:0.77,
-    ajax:0.65,
-    matlab:0.88,
-    da:0.7,
-    ml:0.7,
-    ai:0.75,
-    fem:0.8,
-    cfd:0.85,
-    nodejs:0.67
-  };
+    $(".data").click(function(){
+        $(".info").find(".container").html($(this).find(".loader").html());
+        $(".header").css("border-bottom-color", "#ddd");
+        $(".shape").css("background-color", "#ddd");
+        $(".panel").css("margin-left", "0vw");
+        $(".info").css("margin-left", "15vw");
+        $(".info").find(".container").css("margin-left", "3vw");
+        $(".info").find(".container").find(".pimg").find("p").css("margin-top", "2vw");
+        $(".info").find(".container").find(".others").css("margin-left", "0vw");
+        $(".pname").text($(this).find(".mask").find("div").text() + "");
+        $(".pl").text($(this).find(".mask").find("p").text() + "");
+    });
 
 
-  $scope.skillStyle = {
-    html:{'width':$scope.wbl*$scope.completetion.html + "vw"},
-    css:{'width':$scope.wbl*$scope.completetion.css + "vw"},
-    js:{'width':$scope.wbl*$scope.completetion.js + "vw"},
-    jquery:{'width':$scope.wbl*$scope.completetion.jquery + "vw"},
-    bootstrap:{'width':$scope.wbl*$scope.completetion.bootstrap + "vw"},
-    android:{'width':$scope.wbl*$scope.completetion.android + "vw"},
-    java:{'width':$scope.wbl*$scope.completetion.java + "vw"},
-    c:{'width':$scope.wbl*$scope.completetion.c + "vw"},
-    python:{'width':$scope.wbl*$scope.completetion.python + "vw"},
-    git:{'width':$scope.wbl*$scope.completetion.git + "vw"},
-    ng:{'width':$scope.wbl*$scope.completetion.ng + "vw"},
-    sql:{'width':$scope.wbl*$scope.completetion.sql + "vw"},
-    ajax:{'width':$scope.wbl*$scope.completetion.ajax + "vw"},
-    matlab:{'width':$scope.wbl*$scope.completetion.matlab + "vw"},
-    da:{'width':$scope.wbl*$scope.completetion.da + "vw"},
-    ml:{'width':$scope.wbl*$scope.completetion.ml + "vw"},
-    ai:{'width':$scope.wbl*$scope.completetion.ai + "vw"},
-    fem:{'width':$scope.wbl*$scope.completetion.fem + "vw"},
-    cfd:{'width':$scope.wbl*$scope.completetion.cfd + "vw"},
-    nodejs:{'width':$scope.wbl*$scope.completetion.nodejs + "vw"}
-  };
+    $(".backbtn").click(function(){
+        $(".header").css("border-bottom-color", "#111");
+        $(".shape").css("background-color", "#111");
+        $(".panel").css("margin-left", "-17vw");
+        $(".info").css("margin-left", "100vw");
+        $(".info").find(".container").css("margin-left", "113vw");
+        $(".info").find(".container").find(".pimg").find("p").css("margin-top", "-102vw");
+        $(".info").find(".container").find(".others").css("margin-left", "100vw");
+    });
 
 
-  // $scope.percent = {'width':''+widthbar*0.15+'px'};
-
-
-  $scope.tabClicked = function (id){
-    var cname = id;
-    $scope.name = cname;
-
-
-
-
-
-  };
-
-
-
-  $scope.openPage = function (url){
-    // console.log(url);
-    if(url!=="http://scakc.github.io"){
-      $window.open(""+url );
-    }else{
-      alert('You are on the same page!');
-    }
-  };
-
-}
-
-
-
-})();
+});
